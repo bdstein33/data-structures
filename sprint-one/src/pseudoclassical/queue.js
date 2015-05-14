d218d04bec78007a5189e9ba0de7a1c3ad14d316
Hack Reactor Students <accounts@hackreactor.com>
2015-05-13 18:26:46 -0700

    completed pseudoclassical pattern for both stack & queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,28 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.queueSize = 0;
+  this.storage = {};
+
+
 };
 
+Queue.prototype.size = function() {
+  return this.queueSize;
+};
+
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.queueSize] = value;
+  this.queueSize++;
+};
+
+Queue.prototype.dequeue = function() {
+  if (this.queueSize > 0) {
+    var removed = this.storage[0];
+    for (var i=0; i < this.queueSize - 1; i++) {
+      this.storage[i] = this.storage[i+1];
+    }
+    delete this.storage[this.queueSize - 1];
+    this.queueSize--;
+    return removed;
+  }
+};
 
