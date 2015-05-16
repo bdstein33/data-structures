8e9dc47bb38204f46bc93fa3b8ffbcf915d25266
Hack Reactor Students <accounts@hackreactor.com>
2015-05-15 19:58:35 -0700

    Partial progress on binary search tree insert

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,58 @@
 var BinarySearchTree = function(value){
+  this.storage = {};
+  this.storage[value] = {left: {}, right: {}};
+  this.base = value;
 };
 
+BinarySearchTree.prototype.left = function(){
 
+};
+
+BinarySearchTree.prototype.right = function(){
+
+};
+
+BinarySearchTree.prototype.insert = function(value){
+  // for (var node in this.storage) {
+  //   // if value > node, then add to node[right]
+  //   // otherwise, add to node[left]
+  // }
+  var insertValue = function(node) {
+    var nodeValue;
+    for (var key in node) {
+      nodeValue = key;
+    }
+
+    if (value > nodeValue) {
+      if (node[nodeValue].right === {}) {
+        node[nodeValue].right = {value: {left: {}, right: {}}};
+      } else {
+        insertValue(node[nodeValue].right);
+      }
+    // } else if (value < node) {
+    //   if (node[nodeValue][left] === {}) {
+    //     node[nodeValue][left] = {value: {left: {}, right: {}}};
+    //   } else {
+    //     insertValue(node[left]);
+    //   }
+    }
+  }
+  insertValue(this.storage);
+};
+
+BinarySearchTree.prototype.contains = function(){
+
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(){
+
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+var test = new BinarySearchTree(8);
+console.log(test);
+test.insert(10);
+console.log(test);
+
