f3aa204795e8139c79a67fd9e41e321dc780a922
Hack Reactor Students <accounts@hackreactor.com>
2015-05-16 11:34:38 -0700

    Set up doublyLinkedList files

---
+++ sprint-two/src/doublyLinkedList.js
@@ -0,0 +1,27 @@
+var DoublyLinkedList = function() {
+  this.head = null;
+  this.tail = null;
+  this.list = {};
+};
+
+
+DoublyLinkedList.prototype.addToTail = function(){
+
+};
+
+DoublyLinkedList.prototype.removeHead = function(){
+
+};
+
+DoublyLinkedList.prototype.contains = function(){
+
+};
+
+DoublyLinkedList.prototype.addToHead = function(){
+
+};
+
+DoublyLinkedList.prototype.removeTail = function(){
+
+};
+
